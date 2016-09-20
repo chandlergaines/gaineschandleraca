2d44410a5761831b5004d7d840225bc0f3fd58b0
Chandler Gaines <chandlergaines@gmail.com>
2016-09-20 14:33:58 -0500

    DOES NOT WORK: Yet..

---
+++ DuckDuckGo/app/src/main/java/com/chandler/android/aca/duckduckgo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chandler.android.aca.duckduckgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
