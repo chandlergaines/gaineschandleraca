fe88b92fe916a9515ac88e59c6a3498ba56727d0
Chandler Gaines <chandlergaines@gmail.com>
2016-09-20 14:27:27 -0500

    Initial Commit: Logic written with no user output

---
+++ Hangman/app/src/main/java/com/chandler/android/aca/hangman/Game.java
@@ -0,0 +1,41 @@
+package com.chandler.android.aca.hangman;
+
+public class Game {
+
+    private String mAnswer;
+    private String mHits;
+    private String mMisses;
+    public static final int MAX_MISSES = 7;
+
+    public Game (String answer) {
+        mAnswer = answer;
+        mHits = "";
+        mMisses = "";
+    }
+
+    public boolean applyGuess(char letter){
+        boolean isHit = mAnswer.indexOf(letter) >= 0;
+        if (isHit){
+            mHits += letter;
+        } else {
+            mMisses += letter;
+        }
+        return isHit;
+    }
+
+    public String getCurrentProgress(){
+        String progress = "";
+        for(char letter: mAnswer.toCharArray()){
+            char display = '-';
+            if(mHits.indexOf(letter)>=0){
+                display = letter;
+            }
+            progress += display;
+        }
+        return progress;
+    }
+
+    public int getRemainingTries(){
+        return MAX_MISSES - mMisses.length();
+    }
+}
