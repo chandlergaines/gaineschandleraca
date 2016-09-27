70e530558a1c4f9bb47f3b4b6b245bcaf227a692
Chandler Gaines <chandlergaines@gmail.com>
2016-09-27 11:01:21 -0500

    Fragments and Sliders

--- FragmentSlider/app/src/main/java/com/chandler/android/aca/fragmentslider/SimpleFragment.java
+++ FragmentSlider/app/src/main/java/com/chandler/android/aca/fragmentslider/SimpleFragment.java
@@ -1,4 +1,49 @@
 package com.chandler.android.aca.fragmentslider;
 
-public class SimpleFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SimpleFragment extends Fragment{
+    //Holds the fragment id passed in when created
+    public static final String MESSAGE = "";
+
+    //Our new instance method which we call to make a new Fragment
+    public static SimpleFragment newInstnce(String message)
+    {
+        //Create the fragment
+        SimpleFragment fragment = new SimpleFragment();
+
+        //Create a bundle for our message/id
+        Bundle bundle = new Bundle(1);
+        // Load up the Bundle
+        bundle.putString(MESSAGE, message);
+        // Call setArguments ready for when onCreate is called
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Get the id from the Bundle
+        String message = getArguments().getString(MESSAGE);
+
+        // Inflate the view as normal
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+
+        // Get a reference to textView
+        TextView messageTextView = (TextView)view.findViewById(R.id.textView);
+
+        // Display the id in the TextView
+        messageTextView.setText(message);
+
+        // We could also handle any UI
+        // of any complexity in the usual way
+
+        return view;
+    }
 }
