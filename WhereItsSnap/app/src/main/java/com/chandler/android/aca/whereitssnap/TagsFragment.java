850c98ee1cbe401d853481dcf3b7d821de7338f5
Chandler Gaines <chandlergaines@gmail.com>
2016-09-27 11:02:48 -0500

    Photo gallery and capture app: in progress

---
+++ WhereItsSnap/app/src/main/java/com/chandler/android/aca/whereitssnap/TagsFragment.java
@@ -0,0 +1,14 @@
+package com.chandler.android.aca.whereitssnap;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+
+public class TagsFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ WhereItsSnap/app/src/main/java/com/chandler/android/aca/whereitssnap/TitlesFragment.java
@@ -0,0 +1,13 @@
+package com.chandler.android.aca.whereitssnap;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+public class TitlesFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
